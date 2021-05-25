38ceebd0e879b69a011193a075262fec37bf3e58
wbfw3131 <81532443+wbfw3131@users.noreply.github.com>
2021-05-25 14:24:31 -0600

    Removed unnecessary semicolons

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 import time
-print("Hello there! You can see this.");
-time.sleep(3.5);
-phrase = input("Type something: ", );
-print(phrase + " you! HahAHahAhaHa!!!");
+print("Hello there! You can see this.")
+time.sleep(3.5)
+phrase = input("Type something: ", )
+print(phrase + " you! HahAHahAhaHa!!!")
